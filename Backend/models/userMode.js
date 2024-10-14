b93b9cb5c73536075880496645ac1961ad4a3af4
Pratik <patilpratikp035@gmail.com>
2024-10-15 03:00:54 +0530

    creating models

---
+++ Backend/models/userMode.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    pic: {
+      type: String,
+      require: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timeStamp: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+model.exports = User;
